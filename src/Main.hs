3ccdcc6acc02f2cd2954b199dc854f69cb0dde3c
Akshay Nair <phenax5@gmail.com>
2022-05-24 21:49:17 +0530

    feat: adds spawning process for the keys pressed

--- src/Main.hs
+++ src/Main.hs
@@ -3,11 +3,50 @@ module Main where
 -- import Control.Concurrent (threadDelay)
 
 import Control.Concurrent (threadDelay)
-import Control.Monad (forever)
+import Control.Monad (forever, void)
+import Data.List (find)
+import Sound.PortMidi.Simple (ChannelMessage (..), key)
 import qualified Sound.PortMidi.Simple as Midi
+import qualified System.Process as Proc
+
+data MidiEvent
+  = KeyDown Int
+  | KeyUp Int
+
+data Command
+  = Shell String
+  | Execute ()
+  deriving (Show)
+
+shell :: String -> IO ()
+shell _ = pure ()
+
+handlers :: [(MidiEvent, Command)]
+handlers =
+  [ (KeyDown 44, Shell "dmenu_run"),
+    (KeyDown 51, Shell "echo \"1\n2\n3\n4\n5\" | dmenu -p 'LAMBDA > '"),
+    (KeyDown 48, Shell "st")
+  ]
+
+getCommand :: Midi.ChannelMessage -> Maybe Command
+getCommand = \case
+  NoteOn {key} -> snd <$> find ((\case (KeyDown n) -> key == n; _ -> False) . fst) handlers
+  NoteOff {key} -> snd <$> find ((\case (KeyUp n) -> key == n; _ -> False) . fst) handlers
+  _ -> Nothing
+
+runCommand :: Command -> IO ()
+runCommand = \case
+  Shell cmd -> void . Proc.spawnCommand $ cmd
+  _ -> pure ()
 
 onMessage :: Midi.Message -> IO ()
-onMessage = print
+onMessage = \case
+  Midi.Channel _ msg -> do
+    -- print msg
+    case getCommand msg of
+      Just cmd -> runCommand cmd
+      Nothing -> pure ()
+  _ -> pure ()
 
 main :: IO ()
 main = Midi.withMidi $ do
