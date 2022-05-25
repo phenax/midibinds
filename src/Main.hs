ab7c0bc45853d77a76cfc6d40ca142ce45d64741
Akshay Nair <phenax5@gmail.com>
2022-05-25 15:23:36 +0530

    feat: adds volume and brightness controls

--- src/Main.hs
+++ src/Main.hs
@@ -3,12 +3,31 @@ module Main where
 import Action
 import Control.Concurrent (threadDelay)
 import Control.Monad (forever)
-import Data.List (find)
+import Data.List (find, intercalate)
 import qualified Sound.PortMidi as PM (DeviceInfo (..))
 import qualified Sound.PortMidi.Simple as Midi
 import qualified Sound.PortMidi.Simple as PM
 import qualified System.Environment as Env
 import Utils
+
+alsaSet :: String -> Int -> String
+alsaSet name perc =
+  "amixer sset '" ++ name ++ "' '" ++ show perc ++ "%'"
+
+setVolume :: Int -> String
+setVolume vol = alsaSet "Master" $ toPerc vol
+
+setBrightness :: Int -> String
+setBrightness brightness =
+  "brightnessctl s '" ++ show (toPerc brightness) ++ "%'"
+
+setMicVolume :: Int -> String
+setMicVolume vol =
+  intercalate
+    " && "
+    [ alsaSet "Mic Boost" $ toPerc vol,
+      alsaSet "Internal Mic Boost" $ toPerc vol
+    ]
 
 handlers :: [Handler]
 handlers =
@@ -16,7 +35,9 @@ handlers =
     KeyUp 48 $ Shell $ const "xdotool key super+6",
     KeyUp 51 $ Shell $ const "dmenu_run",
     KeyUp 45 $ Action $ \_ -> print "foobarity",
-    PitchWheel $ Action $ \n -> print n
+    ControlChange 2 $ Shell setVolume,
+    ControlChange 3 $ Shell setBrightness,
+    ControlChange 4 $ Shell setMicVolume
   ]
 
 onMessage :: Midi.Message -> IO ()
--- src/Utils.hs
+++ src/Utils.hs
@@ -1,5 +1,13 @@
 module Utils where
 
+import Data.Function (on)
+
 head' :: [a] -> Maybe a
 head' [] = Nothing
 head' (x : _) = Just x
+
+divF :: Int -> Int -> Float
+divF = (/) `on` fromIntegral
+
+toPerc :: Int -> Int
+toPerc n = (100 * n) `div` 127
