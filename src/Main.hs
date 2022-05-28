5ca938b0400fd4368195080e0d4d73a1d0595671
Akshay Nair <phenax5@gmail.com>
2022-05-28 15:31:34 +0530

    feat: adds proper package version number

--- src/Main.hs
+++ src/Main.hs
@@ -5,9 +5,11 @@ import Config
 import Control.Concurrent (threadDelay)
 import Control.Monad (forM_, forever, (<=<))
 import Control.Monad.IO.Class (liftIO)
+import Data.Version (showVersion)
 import MiMonad
 import Midi
 import qualified Options.Applicative.Simple as Opt
+import Paths_midi_keybindings (version)
 import qualified Sound.PortMidi as PM
 import qualified Sound.PortMidi.Simple as PM
 
@@ -39,9 +41,8 @@ main :: IO ()
 main = do
   let description = "Use your midi controller to runWithMidi commands on your machine"
   let header = "midibind - Midi Keybindings"
-  let version = "0.0.0"
 
-  let runCmd = snd <=< Opt.simpleOptions version header description (pure ())
+  let runCmd = snd <=< Opt.simpleOptions (showVersion version) header description (pure ())
 
   runCmd $ do
     Opt.addCommand
